ddd3f48e992f6ff438c1f7cc0f35d7db41c5e6a6
Vulcanric <jneric49@gmail.com>
2024-03-29 18:54:46 +0100

    Update manifest to install flask requirement, Werkzeug also

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,14 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => pip3,
 }
+
+# Install flask requirement: Werkzeug also
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => pip3,
+}
+
+# The above resource declaration is same as running the following command on the cli
+# $ pip3 install flask==2.1.0
+# $ pip3 install Werkzeug==2.1.1
