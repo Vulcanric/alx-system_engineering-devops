8557c35c3ddfcf12c2802a33f31462a16289d276
Vulcanric <jneric49@gmail.com>
2024-04-04 11:45:06 +0100

    SSH Client configuration using Puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# SSH configuration using puppet
+# Change the first line containing the pattern 'PasswordAuthentication' to 'PasswordAuthentication no'
+# Change the first line containing the pattern 'IdentityFile' to 'IdentityFile ~/.ssh/school'
+
+exec { '/etc/ssh/ssh_config':
+  command => "/bin/sed -i.bak -e '/PasswordAuthentication/ c \
+PasswordAuthentication no' -e '/IdentityFile/ c \
+IdentityFile ~/.ssh/school' /etc/ssh/ssh_config",
+  path    => '/usr/bin:/bin',
+}
